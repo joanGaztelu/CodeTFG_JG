fdb42312679d719506e8210748dcb980eb29d121
Joan Gaztelu <joangaztelu@black-shuck.home>
2020-06-22 20:07:32 +0200

    JGaztelu_CodeTFG

--- Code_tfg.R
+++ Code_tfg.R
@@ -1,132 +1,14 @@
-## Thie fitting code is a part of: Anders, Alario, Van Maanen (2016). The Shifted Wald Distribution for Response Time Data Analysis. Psychological Methods.
+##Code by Joan Gaztelu Camps
 ###########################################################
 
-#Install any missing libraries by using the following commands:
+#load necessary libraries
+library(ggsignif);library(ggplot2);library(plyr);
 
-install.packages("SuppDists");install.packages("Rcpp"); install.packages("plyr"); install.packages("ggplot2");
-
-library(SuppDists); library(plyr); library(ggplot2);  
-
-## swfit() fits one Shifted Wald to a vector of observations 
-swfit <- function(x,type=1,outlier=FALSE,dtype=1,search=FALSE,ql=.01,qu=.99,outl=10,outu=10,criterion=1){
-  # For thorough performance, searches fit performance across a number of ranges
-  if(search==TRUE){
-    quantl <- list(
-      # Insert here to add additional options (quantiles, distance types) to search
-      # quants is the range of quantiles, dtype = 1 for L1-norm, 2 for L2-norm distance  
-      list(quants = seq(.01,.99,len=100), dtype = 1),
-      list(quants = seq(.01,.98,len=100), dtype = 1),
-      list(quants = seq(.01,.97,len=100), dtype = 1),
-      list(quants = seq(.02,.99,len=100), dtype = 1),
-      list(quants = seq(.02,.98,len=100), dtype = 1),
-      list(quants = seq(.03,.97,len=100), dtype = 1),
-      list(quants = seq(.001,.999,len=100), dtype = 1),
-      list(quants = seq(.001,.99,len=100), dtype = 1),
-      list(quants = seq(.01,.999,len=100), dtype = 1)
-    )}; 
-  # For speedier performance, calculates a fit on only one given range
-  if(search==FALSE){quantl <- list(list(quants = seq(ql,qu,len=100), dtype = 1))}
-  
-  calcparam <- function(beta,x){
-    #calcparam(), given a beta value, calculates the other SW parameters
-    # Compute a_0 and an initial estimate of theta
-    a_0 <- ((mean(x) - min(x))^3 / mean((x-mean(x))^2))^.5
-    thetaest <- function(x,a,beta) { n <- length(x);
-    tmpf <- function(i,beta,n) (1-pinvGauss(i,nu=beta,1))^n
-    min(x) - (a^2)*integrate(tmpf, lower=0, upper=Inf, beta,n)$value   }
-    t_init <- thetaest(x,a_0,beta)
-    # Then compute an initial estimate of alpha
-    alphaest <- function(x,t) { (mean((x-t)^-1) - (mean(x)-t)^-1)^-.5 }
-    a_init <- alphaest(x,t_init)
-    # Compute a final estimate of theta
-    t_final <- thetaest(x,a_init,beta)
-    # Compute a final estimate of alpha
-    a_final <- alphaest(x,t_final)
-    # Compute a final estimate of gamma
-    g_final <- 1/(beta*a_final)  
-    return(c(gamma=g_final, alpha=a_final, theta=t_final,mu=beta*(a_final^2), lambda=a_final^2,tau=t_final,beta=beta))
-  }
-  optfunc <- function(beta,x,qnts,dtype=1){
-    # optfunc() is the function that is minimized by the deviance criterion
-    # Given a beta, calcparam is called, then the deviance criterion is calculated
-    # returns the quantile deviation statistic. 
-    vars <- as.list(calcparam(beta=beta,x=x))
-    if(dtype==1){return(sum(abs((qinvGauss(qnts,vars$mu,vars$lambda)+vars$tau)-x)))}
-    if(dtype==2){return(sum(((qinvGauss(qnts,vars$mu,vars$lambda)+vars$tau)-x)^2))}
-  }
-  tmp <- sol <- NULL;
-  for(i in 1:length(quantl)){
-    qnts <- quantl[[i]]$quants; dtype <- quantl[[i]]$dtype; 
-    xq <- quantile(x, qnts,type=7) 
-    sol <- optimize(optfunc, x=xq, qnts=qnts, dtype=dtype, interval=c(.0001,10000), maximum=F)  
-    tmp <- rbind(tmp,c(calcparam(beta=sol$minimum,x=xq),res=sol$objective))
-  }; vars <- data.frame(tmp)
-  
-  # Criterions that will decide which fit (from quantl) to return.
-  quantl2 <- list(
-    # Insert here to add additional criterions, access by e.g. criterion=2 in swapply() 
-    seq(.05,.95,len=91)
-  )
-  xqnts2 <- quantile(x, quantl2[[1]],type=7);
-  pxqnts2 <- data.frame(t(sapply(1:dim(vars)[1],function(x) qinvGauss(quantl2[[criterion]],vars$mu[x],vars$lambda[x])+vars$tau[x]))); 
-  return(unlist(vars[order(rank(
-    rank(apply(abs(sweep(pxqnts2,2,xqnts2)[,1:5]),1,sum))+  # which fits best the first percentiles (5th-10th)
-      rank(apply(abs(sweep(pxqnts2,2,xqnts2)[,1:91]),1,sum)) # which fits most consistently over all percentiles (5th-95th)
-    ,ties.method="random"))[1],])) # return these parameters
-}
-
-## swapply() fits a Shifted Wald to a full data set (e.g. data frame), by each factor specified
-## Two factors minimum, e.g. "condition" and "subject" for one factor, use swfit() with tapply()
-swapply <- function(dat,facs,obsvar = "RT",exclude=NA,type="1",doplot=FALSE,ql=.01,qu=.99,search=TRUE,criterion=1,outlier=NA,outl=3,outu=6){
-  if(!is.na(outlier)){
-    dat2 <- dat; dat <- NULL
-    for(i in unique(dat2[[outlier]])){dat <- rbind(dat,dat2[which(dat2[[outlier]]==i & dat2[[obsvar]] >= median(dat2[[obsvar]][which(dat2[[outlier]]==i)])-(outl*mad(dat2[[obsvar]][which(dat2[[outlier]]==i)])) &  dat2[[obsvar]] <= median(dat2[[obsvar]][which(dat2[[outlier]]==i)])+(outu*mad(dat2[[obsvar]][which(dat2[[outlier]]==i)]))),])}
-  }
-  qnts <- seq(.1,.9,len=9); set.seed(1)
-  dat=ST
-  obsvar="ST$RT2"
-  vars <- with(dat, aggregate(eval(parse(text=obsvar)),
-                              by = lapply((lapply(facs,function(x) parse(text=x))),function(x) as.factor(eval(x))),
-                              FUN = function(x) return(swfit(x,type=type,search=search,ql=ql,qu=qu,criterion=1)))); 
-  
-  dimnames(vars)[[2]][1:length(facs)] <- facs
-  vars <- data.frame(vars[,length(facs)+1],vars[,1:length(facs)]);
-  xqnts <- with(dat, aggregate(eval(parse(text=obsvar)),
-                               by = lapply((lapply(facs,function(x) parse(text=x))),function(x) as.factor(eval(x))), 
-                               FUN = function(x) quantile(x, qnts,type=7))); 
-  dimnames(xqnts)[[2]][1:length(facs)] <- facs; 
-  xqnts <- data.frame(xqnts[,length(facs)+1],xqnts[,1:length(facs)]); 
-  dimnames(xqnts)[[2]][1:length(qnts)] <- round(qnts,3); 
-  #xqnts <- xqnts[,1:length(qnts)]
-  
-  
-  
-  pxqnts <- data.frame(t(sapply(1:dim(vars)[1],function(x) qinvGauss(qnts,vars$mu[x],vars$lambda[x])+vars$tau[x]
-  )));  
-  
-  dimnames(pxqnts)[[2]][1:length(qnts)] <- round(qnts,3)
-  
-  if(doplot==TRUE){
-    swplot(vars=vars,dat=dat,obsvar=obsvar,exclude=exclude) 
-  }
-  return(list(vars=vars,xqnts=xqnts,pxqnts=pxqnts,dat=dat))
-}
-###############
-
-
-#fit SW distributions
-fit<- swapply(dat=ST,search=TRUE, obsvar="RT2", #obsvar = name of RT variable
-              facs=c("ST$Sound","ST$Time", "ST$Subject")) 
-
-#End of fitting method
-
-#Code done by Joan Gaztelu
-
-#code to print subject cell agregated residual plot
+#Print subject residual plot
 #sound options: "Flat" or "Loom"
 analysis_plots<-function(sound){
-  #setwd("directory to store the plots")
-  setwd("/Volumes/PANDORA/Courses/PR/PLOTS/Paleta1/analysis_subject/")
+  setwd("directory to store the plots")
+  
   #unique times in all dataset
   times<-c(unique((ST$Time)))
   
@@ -198,12 +80,13 @@ analysis_plots<-function(sound){
 }
 
 
-#code to plot mean plots
-mean_plots("Loom")
+#Plot mean plots
+mean_plots("Flat")
 mean_plots<-function(sound){
   #setwd("set working directory")
   setwd("/Volumes/PANDORA/Courses/PR/PLOTS/Paleta1/means")
   times<-c(unique((ST$Time)))
+  fit$pxqnts[10:12]<-fit$xqnts[10:12]
   sequence<-c(seq(0.1, 0.9, 0.1))
   new_means_f<-data.frame(sequence)
   new_sd<- data.frame(sequence)
@@ -233,19 +116,19 @@ mean_plots<-function(sound){
     plot_loom<-ggplot(table_M_S) + geom_line(aes(x = factor(Time, level=lev), y=Mean, group= sequence))+
       geom_errorbar(aes(x=factor(Time, level=lev), ymin=Mean-SD, ymax=Mean+SD, group=(sequence), colour=factor(sequence)), width=0.2) +
       geom_point(aes(x = factor(Time, level=lev), y=Mean, group= sequence, color=factor(sequence))) + 
-      theme_bw() + theme(axis.text.x = element_text(angle = 90))+labs(color="Quantiles" ,x="Temporal delay [ms]", y = "Means", title ="Mean Quantiles", subtitle = "Sound Loom" ) + scale_x_discrete(labels = c("No sound", "300", "800", "1500", "2200", "1700"))+ scale_color_viridis_d()
-    print(plot_loom)
+      theme_bw() + theme(axis.text.x = element_text(angle = 90))+labs(color="Quantiles" ,x="Temporal delay [ms]", y = "Means  [ms]", title ="Mean Quantiles", subtitle = "Sound Loom" ) + scale_x_discrete(labels = c("No sound", "300", "800", "1500", "2200", "1700"))+ scale_color_viridis_d()
+    print(plot_loom) 
     ggsave(filename = paste0("Mean", sound, ".png",sep=""), plot = plot_loom, units = "cm")
   }
   else if(sound=="Flat"){
     plot_flat<-ggplot(table_M_S) + geom_line(aes(x = factor(Time, level=lev), y=Mean, group= sequence))+geom_errorbar(aes(x=factor(Time, level=lev), ymin=Mean-SD, ymax=Mean+SD, group=(sequence), colour=factor(sequence)), width=0.2)+ geom_point(aes(x = factor(Time, level=lev), y=Mean, group= sequence, color=factor(sequence))) + 
-      theme_bw()+theme(axis.text.x = element_text(angle = 90)) + labs(color="Quantiles" ,x="Temporal Delay [ms]", y = "Means", title ="Mean Quantiles", subtitle = "Sound Flat")+scale_x_discrete(labels = c("No sound", "300", "800", "1500", "2200", "1700")) + scale_color_viridis_d() 
+      theme_bw()+theme(axis.text.x = element_text(angle = 90)) + labs(color="Quantiles" ,x="Temporal Delay [ms]", y = "Means [ms]", title ="Mean Quantiles", subtitle = "Sound Flat")+scale_x_discrete(labels = c("No sound", "300", "800", "1500", "2200", "1700")) + scale_color_viridis_d() 
     print(plot_flat)
     ggsave(filename = paste0("Mean", sound, ".png",sep=""), plot = plot_flat, units = "cm")
   }
 }
 
-#plots for the parameters
+#Plots for the parameters
 par_plot<- function(sound){
   setwd("/Volumes/PANDORA/Courses/PR/PLOTS/Paleta1/parameters")
   
@@ -343,5 +226,3 @@ par_plot<- function(sound){
   
   ggsave(filename = paste0("_Mean_Theta.png"), plot = thplot, units = "cm")
 }
-
-
